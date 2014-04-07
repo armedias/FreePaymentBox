706d192512a2d5ec921aee2ab2c068f5bd49a8d8
SebSept <sebastienmonterisi@yahoo.fr>
2014-04-07 15:33:09 +0200

    gestion des soumissions sur commandes déjà crées

--- README.md
+++ README.md
@@ -1,31 +1,54 @@
 # FreePaymentBox
 
+Module de paiment PayBox Prestashop 1.5 (1.6).
 
-This is module to use Paybox Payment system for Prestashop.
+Utilise la methode de vérification des données HMAC. (et non pas la methode par CGI).
 
-Module for using Paybox in Prestashop 1.5.
+## Pré-requis
 
-No CGI module required, instead it use the HMAC verification method.
-
-It already works on a real shop with real money but need improvements (admin part at least)
-
-## Requirements
-
-- php (version ?)
-- openssl
-- a test or a real customer account (provided by paybox) 
 - prestashop >= 1.5.2
+- serveur web avec php
+- extension openssl (généralement pré-installée sur les serveurs)
+- un compte client paybox
 
-Documentation, test account and public key are provided by Paybox and are needed. Those are supplied in Paybox download section.
+La documentation et le compte Paybox sont fournis par Paybox.
 
-## Todo
+## Etat
 
-Admin part of the module is pretty ugly at this time
-- mode_prod : 0 is for test, 1 is for production
-- pbx_hash : is to be set to SHA512
+Ce module est utilisé en production, il est fonctionnel.
+
+## Services
+
+Je peux installer ce module et le paramétrer pour vous. [Contactez moi](http://seb7.fr).
+
+## Merci
+
+Si vous utilisez ce module, vous pouvez me remercier en me gratiffiant d'un petit quelque chose : 
+[Merci](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BNDQYWMZVUNES)
+
+## Todo - Contribuez
+
+- formulaire d'administration
+- refactorisation du code (intégration dans le module)
+- basculement vers le serveur de secours paybox
+- Paramétrer le niveau de log
+- ... pas mal de choses sont faisables
 
 
 ## DISCLAIMER
+
+### fr
+
+Ce module n'est pas réalisé par PayBox et n'est pas lié Point Transaction System.
+Prestashop n'est pas lié au projet.
+
+Prestashop, Paybox et Point Transaction Systems sont des marques déposées.
+
+Pour des raisons légales, ce module est fourni sans aucune garanti. Les developpeurs ne pourrons en aucun cas être tenus responsables si d'eventuelles dysfonctionnements ou perturbations liés au module pouvaient survenir.
+
+Ce module est publié sous [Licence GPL v3](http://www.gnu.org/licenses/gpl-3.0.txt).
+
+### en
 
 This module is NOT made by Paybox and has no relation with Point Transaction Systems. 
 Prestashop is NOT involved in this project.
--- ipn.php
+++ ipn.php
@@ -30,10 +30,17 @@ require_once(dirname(__FILE__) . '/freepaymentbox.php');
 $success = true;
 
 /**
+ * Comptage des erreurs
+ * Pour déterminer si l'echec du paiement n'est du qu'au code erreur ou pas
+ * @todo gérer autrement
+ */
+$errors_count = 0;
+
+/**
  * Message enregistré avec la commande / dans le log
  * @var string
  */
-$message;
+$message = '';
 
 // ensemble param url (pour debugage/surveillance)
 $param_url = '';
@@ -57,14 +64,16 @@ $id_cart = (int) $id_cart;
 
 $cart = new Cart($id_cart);
 
-// --- verifications ---
+// --- verifications internes ---
 
 // verification que cart retrouvé
 if(!Validate::isLoadedObject($cart)){
     Logger::addLog('Paiement Paybox (Freepaymentbox) : Paiement sur Cart inexistant ! '.$param_url , 4);
     echo '<html><head></head><body>Erreur 1</body></html>'; 
-    exit(1);
+    exit();
 }
+
+// --- vérification validité du paiement --- (cf doc paybox)
 
 // vérification signature
 if(!Freepaymentbox::verification_signature()){
@@ -78,13 +87,13 @@ if(is_null($param_autorisation))
 }
 
 // vérification code erreur
-if($param_erreur != '00000 ')
+if($param_erreur !== '00000')
 {
-    miseEnEchec("Echec de la transaction. Code erreur $param_erreur");
+    miseEnEchec("Echec de la transaction. Le client n'a pas payé. Code erreur $param_erreur");
 }
 
 // vérification du montant - montant payé = montant de la commande
-if($param_montant != ($cart->getOrderTotal(true) *100) )
+if( $param_montant != (int)($cart->getOrderTotal(true)*100) )
 {
     miseEnEchec('Montants paiement et panier différents (panier: '.$cart->getOrderTotal(true).' payé: .'.($param_montant/100).')');
 }
@@ -96,38 +105,68 @@ if($param_autorisation === 'XXXXXX')
      $message .= 'Paiement fictif ( mode test )';
 }
 
-// enregistrement de la commande
-if($success) {
-    $order_state = Configuration::get('PS_OS_PAYMENT');
-    $message .= 'Paiement validé. Paramètres reçus : '.$param_url;
-    Logger::addLog($message.' / '.$param_url , 1);
+// traitement de l'existance d'une commande déjà placée avec ce cart, si données reçue valides
+
+// commande déjà enregistrée - cas existant ?
+if($cart->OrderExists())
+{
+    // si une seule erreur, si il s'agit du param erreur et que $param_erreur correspond a l'erreur 'paiement déjà effectué'), on ignore cette réponse serveur
+    if( ($errors_count == 1 && $param_erreur === '00015') 
+         ||  $errors_count == 0  
+            )
+    {
+        $message .= 'Retour serveur (1) sur panier/commande existante. '.$param_url;
+        Logger::addLog($message.' / '.$param_url , 2);
+        
+        // Affichage d'un erreur pour être averti par PayBox
+        echo '<html><head></head><body>Erreur 4</body></html>';
+        exit();
+    }
+    else
+    {
+        $message .= 'Retour serveur (2) sur panier/commande existante. '.$param_url;
+        Logger::addLog($message.' / '.$param_url , 2);
+        // Affichage d'un erreur pour être averti par PayBox
+        echo '<html><head></head><body>Erreur 3</body></html>';
+        exit();
+    }
 }
-else {
-    $order_state = Configuration::get('PS_OS_ERROR');
-}
+else // enregistrement de la commande
+{
+    if($success) {
+        $order_state = Configuration::get('PS_OS_PAYMENT');
+        $message .= ' Paiement validé. Paramètres reçus : '.$param_url;
+        Logger::addLog($message.' / '.$param_url , 1); 
+    }
+    else {
+        $order_state = Configuration::get('PS_OS_ERROR');
+        $message .= ' Paiement non validé. Paramètres reçus : '.$param_url;
+        Logger::addLog($message.' / '.$param_url , 4); 
+    }
 
-$payment_module = new Freepaymentbox();
+    $payment_module = new Freepaymentbox();
 
-$payment_module->validateOrder(
-        $id_cart,           // $id_cart
-        $order_state,    // $id_order_state
-        $param_montant/100, 
-        'Paybox (Freepaymentbox)', 
-        $message,
-        array('transaction_id' => $param_ref_cmd),
-        null, //$currency_special
-        false, // $dont_touch_amount
-        $cart->secure_key ? $cart->secure_key : false  // $secure_key - in case there is no secure_key in cart, set to false to validate order anyway
-    );
-
-
-if($success) {  
-    // réponse par page html vide 
+    $validate = $payment_module->validateOrder(
+            $id_cart,           // $id_cart
+            $order_state,    // $id_order_state
+            $param_montant/100, 
+            'Paybox (Freepaymentbox)', 
+            $message,
+            array('transaction_id' => $param_ref_cmd),
+            null, //$currency_special
+            false, // $dont_touch_amount
+            $cart->secure_key ? $cart->secure_key : false  // $secure_key - in case there is no secure_key in cart, set to false to validate order anyway
+        );
+    if(!$validate)
+    {
+        miseEnEchec('Echec ValidateOrder', 5);
+        echo '<html><head></head><body>Erreur 2</body></html>'; 
+        echo $message;
+        exit();
+    }
     echo '<html><head></head><body></body></html>';
-} else {
-    // réponse par page html avec message erreur (provoque envoi de mail par paybox (non vérifié)) 
-   echo '<html><head></head><body>Erreur 2</body></html>'; 
 }
+
 
 /**
  * fonctions locales
@@ -144,16 +183,19 @@ if($success) {
  * 
  * @global $message
  * @global $success
+ * @global $errors_count
  * @global $param_url
  * @param string $message
  * @param int    $niveau_erreur niveau d'erreur pour le Logger
  */
 function miseEnEchec($p_message, $p_niveau_erreur=2)
 {
-    global $success, $message, $param_url;
+    global $success, $message, $errors_count, $param_url;
+    
     // modif var globales
     $success = false;
     $message .= $p_message;
+    $errors_count++;
     
     // enregistrement dans le log PS
     Logger::addLog($p_message.' / '.$param_url , 4);
