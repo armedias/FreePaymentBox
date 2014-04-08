b809ec284c1a5ccb435937636a146bf2f5eb0fe8
SebSept <sebastienmonterisi@yahoo.fr>
2014-04-08 10:26:02 +0200

    just format

--- freepaymentbox/controllers/front/customerreturn.php
+++ freepaymentbox/controllers/front/customerreturn.php
@@ -1,31 +1,43 @@
 <?php
 
-class FreepaymentboxCustomerreturnModuleFrontController extends ModuleFrontController {
-	public $display_column_left = false;
-	public function initContent() {
-		parent::initContent();
+/**
+ * Controlleur Retour du client sur le site
+ * 
+ * @author Sébastien Monterisi   <sebastienmonterisi@yahoo.fr>  https://github.com/SebSept/FreePaymentBox   
+ * @author ?@?                   <?>                            https://github.com/PrestaMath
+ */
+class FreepaymentboxCustomerreturnModuleFrontController extends ModuleFrontController
+{
 
-                // 
-                $status = Tools::getValue('status');
-                
-                switch ($status){
-                    case 'PBX_REFUSE' :
-                        $msg = 'Une erreur est survenue lors du paiement';
-                        break;
-                    case 'PBX_EFFECTUE' :
-                        $msg = 'Votre paiement est en cours de traitement';
-                        break;
-                    case 'PBX_ANNULE' :
-                        $msg = 'Votre paiement est annulée';
-                        break;
-                    default :
-                        $msg = 'Une erreur est survenue lors du paiement';
-                        break;
-                }
-                        
-		$this->context->smarty->assign('msg', $msg);
-		
-		$this->setTemplate('pbxcustomerreturn.tpl');
-	}
+    public $display_column_left = false;
+
+    public function initContent()
+    {
+        parent::initContent();
+
+        // 
+        $status = Tools::getValue('status');
+
+        switch ($status) {
+            case 'PBX_REFUSE' :
+                $msg = 'Une erreur est survenue lors du paiement';
+                break;
+            case 'PBX_EFFECTUE' :
+                $msg = 'Votre paiement est en cours de traitement';
+                break;
+            case 'PBX_ANNULE' :
+                $msg = 'Votre paiement est annulée';
+                break;
+            default :
+                $msg = 'Une erreur est survenue lors du paiement';
+                break;
+        }
+
+        $this->context->smarty->assign('msg', $msg);
+
+        $this->setTemplate('pbxcustomerreturn.tpl');
+    }
+
 }
+
 ?>
